85e1b5b35046d94d6867a3a06f28efc13f537c1a
chrisgins <christiansgina@gmail.com>
2020-02-11 12:05:55 -0700

    mvp = complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,16 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('x is %d, y is %f, z is %s' %(x,y,z))
+# % is a str placeholder, d is placeholder for num/dec values
+# s is to specify format text, f is f-string (literal str interpolation)
+
 # Use the 'format' string method to print the same thing
 
+print('x is {0}, y is {1}, z is {2}' .format(x,y,z))
+#format - for concatenating elements within a str
+# {} defines (or maps) one value to another
+
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y}, z is {z}')
+#f string = literal interpolation
